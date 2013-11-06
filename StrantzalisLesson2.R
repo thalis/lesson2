ad3be06ac164f0524044d840982349dfb7dc2532
thalis <athanasios@strantzalis@wur.nl>
2013-11-07 00:05:05 +0100

    first commit - Test

---
+++ StrantzalisLesson2.R
@@ -0,0 +1,16 @@
+
+is.leap<-function(x){
+  if(x%%400==0){
+    output=TRUE
+  }
+  else if(x%%100==0){
+    output=FALSE
+  }
+  else if(x%%4==0){
+    output=TRUE
+  }
+  else{
+    output= FALSE
+  }
+  return(output)
+}
